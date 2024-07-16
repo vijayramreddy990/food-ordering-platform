96d7364ac0627ee30cc5d8cb5fc6c5a4c7ae484e
vijay <vijayramreddy990@gmail.com>
2024-07-16 22:52:14 +0600

    Main-Nav implementation

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -14,6 +15,9 @@ const Header = () => {
         </Link>
         <div className="md:hidden">
           <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
